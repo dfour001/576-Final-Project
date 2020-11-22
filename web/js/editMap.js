6aadeb6a0ec2175498d1082410447a9443e9afb0
dfour001 <53315984+dfour001@users.noreply.github.com>
2020-11-22 00:04:53 -0500

    Add Image forms and buttons

--- web/js/editMap.js
+++ web/js/editMap.js
@@ -1,9 +1,6 @@
+// Main Map
 var map = L.map('editMap', {zoomControl:false}).setView([37.54, -77.43], 13);
 new L.Control.Zoom({ position: 'topright' }).addTo(map);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
 
 var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -13,8 +10,19 @@ var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/to
 	ext: 'png'
 }).addTo(map);
 
-/*
-map.on('click', function(e) {
-    alert(e.latlng);
-})
-*/
+
+// Image Form Map
+var imgMap = L.map('editFormMap', {
+    zoomControl: false,
+    attributionControl: false,
+    dragging: false,
+    scrollWheelZoom: false,
+    
+}).setView([37.54, -77.43], 13);
+var Stamen_TonerLite2 = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
+}).addTo(imgMap);
--- web/js/editor.js
+++ web/js/editor.js
@@ -1,1 +1,67 @@
-$('#editFormModal-Image').modal('show');
+// Global variables controlling editing
+var insertImg = false; // If true, clicking map will insert image marker
+var insertSlideshow = false; // If true, clicking map will insert slideshow marker
+function reset_insert_tools() {
+    insertImg = false;
+    insertSlideshow = false;
+    $('#editMap').css('cursor', 'grab');
+}
+
+var notTimer; // Notification bar timeout object
+function map_notification(message) {
+    // Cancel previous timout if exists
+    if (notTimer != null) {
+        clearTimeout(notTimer);
+    }
+    
+    // Set and display notification
+    let mNot = $('#mapNotification');
+    mNot.slideDown();
+    mNot.html(message);
+
+    // Hide notification after 6 seconds
+    notTimer = setTimeout(function () {
+        mNot.slideUp();
+        notTimer = null;
+    }, 6000);
+}
+
+
+$('#btnAddImage').on('click', function () {
+    reset_insert_tools();
+    insertImg = true;
+    
+    $('#editMap').css('cursor', 'crosshair');
+    
+    map_notification("Click on a location in the map to place an <span class='color-gold'>image</span> marker");
+});
+
+$('#btnAddSlideshow').on('click', function () {
+    reset_insert_tools();
+    insertSlideshow = true;
+    
+    $('#editMap').css('cursor', 'crosshair');
+    
+    map_notification("Click on a location in the map to place a <span class='color-gold'>slideshow</span> marker");
+});
+
+// Event to insert image or slideshow markers
+map.on('click', function (e) {
+    if (insertImg) {
+        // Show insert image form
+        $('#editFormModal-Image').modal('show');
+
+        // Fix issue with displaying map within a modal
+        setTimeout(function () {
+            imgMap.invalidateSize();
+        }, 250);
+
+        reset_insert_tools();
+        $('#mapNotification').slideUp();        
+    };
+
+    if (insertSlideshow) {
+        alert("Insert Slideshow");
+        $('#mapNotification').slideUp();
+    }
+});
